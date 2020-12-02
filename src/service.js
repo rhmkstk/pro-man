ba979dad5e0c2c8c2eb4808b25cf5d48054957cc
rhmkstk <rahmi.kstk@gmail.com>
2020-12-02 04:14:01 +0300

    fixed baseUrl and added new function

--- src/service.js
+++ src/service.js
@@ -1,34 +1,45 @@
 import axios from 'axios'
 
-const URL = 'https://takenote-d0374.firebaseio.com/user2/workspaces.json'
+const BaseURL = 'https://takenote-d0374.firebaseio.com/user2/workspaces'
 
 export default {
   saveNewWorkspace(ws) {
+    const URL = `${BaseURL}.json`
     return axios.post(URL, ws)
   },
   getWorkspaces() {
+    const URL = `${BaseURL}.json`
     return axios.get(URL)
   },
   deleteWorkspace(key) {
-    const trimUrl = URL.substr(0, URL.length - 5)
-    const deleteURL = `${trimUrl}/${key}.json`
+    const deleteURL = `${BaseURL}/${key}.json`
     return axios.delete(deleteURL)
   },
   editWorkspace(editedWorkspace) {
-    const trimUrl = URL.substr(0, URL.length - 5)
-    const editURL = `${trimUrl}/${editedWorkspace.key}.json`
+    const editURL = `${BaseURL}/${editedWorkspace.key}.json`
     return axios.put(editURL, editedWorkspace)
   },
 
   // CARD TRANSACTİONS
+
   getCards(key) {
-    const trimUrl = URL.substr(0, URL.length - 5)
-    const editURL = `${trimUrl}/${key.key}/cards.json`
+    const editURL = `${BaseURL}/${key.key}/tasks.json`
     return axios.get(editURL)
   },
   addNewCard(card) {
-    const trimUrl = URL.substr(0, URL.length - 5)
-    const addURL = ` ${trimUrl}/${card.key}/cards.json `
+    const addURL = ` ${BaseURL}/${card.key}/tasks/${card.i}/cards.json `
     return axios.post(addURL, card.newCard)
-  }
+  },
+  deleteCard(infos) {
+    const cardDeleteURL = `${BaseURL}/${infos.activeWsKey}/tasks/${infos.stage}/cards/${infos.cardKey}.json`
+    return axios.delete(cardDeleteURL)
+  },
+  moveCard(infos) {
+    const addURL = ` ${BaseURL}/${infos.activeWsKey}/tasks/${infos.stage}/cards.json `
+    return axios.post(addURL, infos.card)
+  },
+  updateCardInfos(infos) {
+    const reOrderURL = ` ${BaseURL}/${infos.activeWsKey}/tasks/${infos.stage}/cards/${infos.card.key}.json `
+    return axios.put(reOrderURL, infos.card)
+  },
 }
