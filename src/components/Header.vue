222330fa74c34e170b9f8ebb7afb1c13fbcb25cc
rhmkstk <rahmi.kstk@gmail.com>
2020-12-02 04:29:59 +0300

    Header completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,61 @@
 <template>
   <header class="header">
-    <div>Selam</div>
-    <IconAdd/>
-    <IconMore/>
-    <IconGarbage/>
-    <IconEdit />
-    <IconArrowLeft/>
+    <IconArrowLeftRound
+      stroke="#fff"
+      class="back"
+      @click="goBack"
+      v-if="isRouteWorkspace"
+    />
+    <span class="activeWs">{{ activeWorkspaceName }}</span>
+    <button>LOGOUT</button>
   </header>
 </template>
 
 <script>
-import IconAdd from './icons/add.svg'
-import IconMore from './icons/more.svg'
-import IconGarbage from './icons/garbage.svg'
-import IconEdit from './icons/edit.svg'
-import IconArrowLeft from './icons/arrowLeft.svg'
-
+import IconArrowLeftRound from './icons/arrowLeftRound.svg'
 
 export default {
   name: 'Header',
   components: {
-    IconAdd,
-    IconMore,
-    IconGarbage,
-    IconEdit,
-    IconArrowLeft
-  }
+    IconArrowLeftRound
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
+  },
+  computed: {
+    isRouteWorkspace() {
+      return this.$route.name != 'HandleWorkspaces'
+    },
+    activeWorkspaceName(){
+      return this.$route.params.name
+    }
+  },
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.header {
+  background-color: var(--c-dark);
+  color: var(--c-text);
+  min-height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+.back {
+  cursor: pointer;
+}
+.header span {
+  font-size: var(--fs-title);
+}
+.header button {
+  color: inherit;
+  font-size: var(--fs-title);
+}
 </style>
