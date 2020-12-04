40af54af71df3fd665e69bf4ad7562017b783958
rhmkstk <rahmi.kstk@gmail.com>
2020-12-05 01:50:40 +0300

    components path edited

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -8,18 +9,39 @@ const router = new VueRouter({
     {
       name: 'HandleWorkspaces',
       path: '/handleworkspaces',
-      component: () =>
-        import(
-          /* webpackChunkName: "dentisAccount" */ '@/components/HandleWorkspaces'
-        )
+      component: () => import('@/views/handleWorkspaces'),
+      beforeEnter: (to, from, next) => {
+        if (store.getters.isUserExist) next()
+        else next({ name: 'Login' })
+      }
     },
     {
       name: 'Workspaces',
       path: '/workspace/:name',
-      component: () =>
-        import(
-          /* webpackChunkName: "dentisAccount" */ '@/components/Workspace'
-        )
+      component: () => import('@/views/workspace'),
+      beforeEnter: (to, from, next) => {
+        if (store.getters.isUserExist) next()
+        else next({ name: 'Login' })
+      }
+    },
+    {
+      name: 'Login',
+      path: '/',
+      component: () => import('@/views/login'),
+      beforeEnter: (to, from, next) => {
+        if (!store.getters.isUserExist) next()
+        else next({name: 'HandleWorkspaces'})
+        // else next({ name: `${from.name}` })
+      }
+    },
+    {
+      name: 'NotFound',
+      path: '/notFound',
+      component: () => import('@/views/notFound'),
+    },
+    {
+      path:'*',
+      redirect:{name: 'NotFound'}
     }
   ],
   mode: 'history'
